6346c2362f7b563719ebc7367f72084906d7c8cf
edU <131560672+Cad-prof@users.noreply.github.com>
2026-03-15 17:42:28 -0100

    feat: add listProducts method (v0.1.0)

--- src/main/java/sn/isi/l3gl/core/service/ProductService.java
+++ src/main/java/sn/isi/l3gl/core/service/ProductService.java
@@ -1,24 +1,27 @@
 package sn.isi.l3gl.core.service;
 
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import sn.isi.l3gl.core.entity.Product;
 import sn.isi.l3gl.core.repository.ProductRepository;
+import java.util.List;               // ← ajouter cet import
 
-@Service                   
-@RequiredArgsConstructor   
+@Service
+@RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    // v0.0.1 
-    /**
-     * Ajoute un nouveau produit dans l'inventaire.
-     * @paramproductLe produit à enregistrer
-     * @return Le produit enregistré (avec son ID généré)
-     */
+    //  v0.0.1 
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    // v0.1.0 
+    /**
+     * Retourne la liste de tous les produits en base.
+     */
+    public List<Product> listProducts() {
+        return productRepository.findAll();
     }
 }
